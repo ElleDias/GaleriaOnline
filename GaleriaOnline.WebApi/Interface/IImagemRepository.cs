458b036096f3d7eca0aa35c44e9aa5c84e6f783f
Danielle Dias <danielle.c.santos9@aluno.senai.br>
2025-08-19 16:48:45 -0300

    :tada: adicionando funcoes ao projeto

---
+++ GaleriaOnline.WebApi/Interface/IImagemRepository.cs
@@ -0,0 +1,15 @@
+﻿using GaleriaOnline.WebApi.Model;
+
+namespace GaleriaOnline.WebApi.Interface
+{
+    public interface IImagemRepository
+    {
+        Task<IEnumerable<Imagem>> GetAllAsync();
+        Task<Imagem> GetByIdAsync(int id);
+        Task<Imagem> CreateAsync(Imagem imagem);
+
+        Task<Imagem> UpdateAsync(Imagem imagem);
+
+        Task<Imagem> DeleteAsync(int id);
+    }
+}
